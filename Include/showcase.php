343046468d7f89b381eaffb1175a1a5d30ca660d
CooperZA <zach@raincitysolutions.com>
2019-08-17 15:23:45 -0700

    Resume and CV are pulled from Onedrive

--- Include/showcase.php
+++ Include/showcase.php
@@ -6,8 +6,8 @@
                 <p>
                     I am a creative problem solver, passionate about web development and data analytics, working towards a degree in Management Information Systems with a background in the outdoors industry.
                 </p>
-                <a href="Assets\Zach_Cooper_Resume.docx" download="ZachCooper_Resume.docx" class="btn btn-info"><i class="fas fa-download"></i> Resume</a>
-                <a href="Assets/CV.docx" download="ZachCooper_CV.docx" class="btn btn-info"><i class="fas fa-download"></i> CV</a>
+                <a href="https://onedrive.live.com/embed?cid=20D43CC72FF9A07D&resid=20D43CC72FF9A07D%21139&authkey=APUtoqKquH4W938&em=2" download="ZachCooper_Resume.docx" class="btn btn-info"><i class="fas fa-download"></i> Resume</a>
+                <a href="https://onedrive.live.com/embed?cid=20D43CC72FF9A07D&resid=20D43CC72FF9A07D%21388&authkey=AGly2rYwK79Jy9E&em=2" download="ZachCooper_CV.docx" class="btn btn-info"><i class="fas fa-download"></i> CV</a>
             </div><!-- End of showcase-text-items -->
         </div><!-- End of showcase-text -->
     </div><!-- End of showcase -->
