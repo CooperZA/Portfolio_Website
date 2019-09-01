5c848e13a5cb5ee9ae6879db8e43291e4fef0701
CooperZA <zach@raincitysolutions.com>
2019-08-31 19:09:46 -0700

    update

--- index.php
+++ index.php
@@ -14,25 +14,6 @@
     <!-- <link rel="stylesheet" href="css\baguettebox.css"> -->
     <link rel="stylesheet" href="css\style.css">
 
-    <!-- Favicon -->
-    <!--<link rel="apple-touch-icon" sizes="57x57" href="Images/Favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="Images/Favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="Images/Favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="Images/Favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="Images/Favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="Images/Favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="Images/Favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="Images/Favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="Images/Favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="Images/Favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="Images/Favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="Images/Favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="Images/Favicon/favicon-16x16.png">
-    <link rel="manifest" href="Images/Favicon/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff"> -->
-
     <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
@@ -51,7 +32,7 @@
     <?php include('include/about.php'); ?>
 
     <!-- Project Carousel -->
-    <?php include('Include\proj_carousel.php'); ?>
+    <?php include('include\proj_carousel.php'); ?>
 
     <!-- Experience -->
     <?php include('include/experience.php'); ?>
